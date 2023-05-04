97cec6602f90625d4b63ff93d57dc1c6980f11cb
MarkoVVV <sam.samlin@mail.ru>
2023-05-04 13:11:18 +0300

    test

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ app.use(bodyParser.json())
 
 // 'vladvald47@gmail.com'
 // 'qlwrgjlhmmopmafu'
+//"headers": [
+//     {
+//       "source": "/api/(.*)",
+//       "headers": [
+//         { "key": "Access-Control-Allow-Credentials", "value": "true" },
+//         { "key": "Access-Control-Allow-Origin", "value": "*" },
+//         { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+//         { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+//       ]
+//     }
+//   ],
 let smtpLogin = process.env.SMTP_LOGIN || '...' ;
 let smtpPassword = process.env.SMTP_PASSWORD || '...' ;
 
